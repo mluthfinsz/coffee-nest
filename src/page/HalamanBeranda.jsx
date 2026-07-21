fda347f3897fa87d64276c63da7a60c676907159
mluthfinsz <165624021+mluthfinsz@users.noreply.github.com>
2026-07-21 21:55:28 +0700

    Update HalamanBeranda.jsx

--- src/page/HalamanBeranda.jsx
+++ src/page/HalamanBeranda.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
-import chatIcon from "../assets/image/chatbot.png";
+// import chatIcon from "../assets/image/chatbot.png";
 
-import Chatbot from "react-chatbot-kit";
-// import "react-chatbot-kit/build/main.css";
-import config from "../chatbot/config";
-import MessageParser from "../chatbot/MessageParser";
-import ActionProvider from "../chatbot/ActionProvider";
+// import Chatbot from "react-chatbot-kit";
+// // import "react-chatbot-kit/build/main.css";
+// import config from "../chatbot/config";
+// import MessageParser from "../chatbot/MessageParser";
+// import ActionProvider from "../chatbot/ActionProvider";
 
-import Navbar from "../component/ComponentUser/UserNavbar";
-import CardHome from "../component/ComponentUser/UserCardHome";
-import Service from "../component/ComponentUser/UserService";
-import AboutProduct from "../component/ComponentUser/UserTentangProduk";
-import Carousels from "../component/ComponentUser/UserFavorit";
-import Footer from "../component/ComponentUser/UserFooter";
-import ReviewCarousel from "../component/ComponentUser/UserReviewCarousel";
-import ReviewForm from "../component/ComponentUser/UserReviewForm";
-import Mobile from "../component/ComponentUser/UserMobile";
+// import Navbar from "../component/ComponentUser/UserNavbar";
+// import CardHome from "../component/ComponentUser/UserCardHome";
+// import Service from "../component/ComponentUser/UserService";
+// import AboutProduct from "../component/ComponentUser/UserTentangProduk";
+// import Carousels from "../component/ComponentUser/UserFavorit";
+// import Footer from "../component/ComponentUser/UserFooter";
+// import ReviewCarousel from "../component/ComponentUser/UserReviewCarousel";
+// import ReviewForm from "../component/ComponentUser/UserReviewForm";
+// import Mobile from "../component/ComponentUser/UserMobile";
 
 import "./style.css";
 
@@ -41,43 +41,50 @@ const Beranda = () => {
     setShowChatbot(!showChatbot);
   };
 
+//   return (
+//     <div style={backgroundStyle}>
+//       <Navbar />
+//       <CardHome />
+//       <Service />
+//       <AboutProduct />
+//       <Mobile />
+//       <Carousels />
+//       <ReviewForm />
+//       <ReviewCarousel />
+//       <Footer />
+//       <div className="chatbot-button">
+//         {showChatbot && (
+//           <div className="chatbot-widget-container">
+//             <div className="chatbot-widget-header">
+//               CoffeeBot
+//               <button onClick={toggleChatbot} className="chatbot-close-button">
+//                 X
+//               </button>
+//             </div>
+//             <div className="chatbot-widget-body">
+//               <Chatbot
+//                 config={config}
+//                 messageParser={MessageParser}
+//                 actionProvider={ActionProvider}
+//               />
+//             </div>
+//           </div>
+//         )}
+//         {!showChatbot && (
+//           <button onClick={toggleChatbot} className="chatbot-button">
+//             <img src={chatIcon} alt="Chatbot Icon" className="chatbot-icon" />
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
   return (
-    <div style={backgroundStyle}>
-      <Navbar />
-      <CardHome />
-      <Service />
-      <AboutProduct />
-      <Mobile />
-      <Carousels />
-      <ReviewForm />
-      <ReviewCarousel />
-      <Footer />
-      <div className="chatbot-button">
-        {showChatbot && (
-          <div className="chatbot-widget-container">
-            <div className="chatbot-widget-header">
-              CoffeeBot
-              <button onClick={toggleChatbot} className="chatbot-close-button">
-                X
-              </button>
-            </div>
-            <div className="chatbot-widget-body">
-              <Chatbot
-                config={config}
-                messageParser={MessageParser}
-                actionProvider={ActionProvider}
-              />
-            </div>
-          </div>
-        )}
-        {!showChatbot && (
-          <button onClick={toggleChatbot} className="chatbot-button">
-            <img src={chatIcon} alt="Chatbot Icon" className="chatbot-icon" />
-          </button>
-        )}
-      </div>
-    </div>
-  );
-};
+  <div style={backgroundStyle}>
+    <Navbar />
+    <Footer />
+  </div>
+);
 
 export default Beranda;
